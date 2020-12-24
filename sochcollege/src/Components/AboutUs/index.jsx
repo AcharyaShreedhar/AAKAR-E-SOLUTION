e4daac254a38bb0e3357869b4a667305f1922579
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-24 20:23:52 +0545

    [SOC-10]aboutus component

---
+++ sochcollege/src/Components/AboutUs/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+import "./AboutUs.scss"
+
+class AboutUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return <div>About Us</div>
+  }
+}
+export default AboutUs;
