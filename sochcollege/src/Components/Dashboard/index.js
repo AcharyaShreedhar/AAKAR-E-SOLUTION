2508d1977b4a1c63cbf7122d9dd0d8d534caedd6
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-24 07:56:31 +0545

    updating the dashboard component with soch logo and menus

--- sochcollege/src/Components/Dashboard/index.js
+++ sochcollege/src/Components/Dashboard/index.js
@@ -1,4 +1,10 @@
+/**
+ * Created by Shreedhar Acharya
+ * 12/23/2020
+ */
 import React, { Component } from "react";
+import logo from "../../images/logo.png";
+import "./Dashboard.css";
 
 class Dashboard extends Component {
   constructor(props) {
@@ -9,7 +15,43 @@ class Dashboard extends Component {
   }
 
   render() {
-    return <div>Dashboard</div>;
+    return (
+      <div className="container d-flex align-items-center">
+        <h1 className="logo mr-auto">
+          <img className="background" src={logo} alt="Soch College" />
+        </h1>
+
+        <nav className="nav-menu d-none d-lg-block">
+          <ul>
+            <li className="active">
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About Us</a>
+            </li>
+            <li>
+              <a href="#">Programmes</a>
+            </li>
+            <li>
+              <a href="#">Our Faculties</a>
+            </li>
+            <li>
+              <a href="#">News and Events</a>
+            </li>
+            <li>
+              <a href="#">Gallery</a>
+            </li>
+            <li>
+              <a href="#">Contact Us</a>
+            </li>
+          </ul>
+        </nav>
+
+        <a href="#" className="get-started-btn">
+          Get Started
+        </a>
+      </div>
+    );
   }
 }
 export default Dashboard;
