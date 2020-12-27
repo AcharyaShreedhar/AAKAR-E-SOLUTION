630899e2823a99cce058320b09c9ec803d895c32
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-27 13:52:22 +0545

    [SOC-11]update title Programme

--- sochcollege/src/Components/Programmes/index.jsx
+++ sochcollege/src/Components/Programmes/index.jsx
@@ -13,9 +13,9 @@ class Programme extends Component {
       <div>
         <div className="d-flex card">
           <div className="d-flex p-3 bg-secondary">
-            <strong>Testimonials</strong>
+            <strong>Programme</strong>
           </div>
-          <p className="text-left color-light p-2">WHAT ARE THEY SAYING</p>
+          <p className="text-left color-light p-2">B.Sc.CSIT</p>
         </div>
       </div>
     );
