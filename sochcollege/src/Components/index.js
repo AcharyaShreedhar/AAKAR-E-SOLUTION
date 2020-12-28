3851eb32905db56ea25ecddd753131270a752909
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-28 21:24:09 +0545

    [SOC-12]import export component

--- sochcollege/src/Components/index.js
+++ sochcollege/src/Components/index.js
@@ -1,5 +1,6 @@
 import Dashboard from "./Dashboard";
 import AboutUs from "./AboutUs";
 import Programme from "./Programmes";
+import NewsAndEvents from "./NewsAndEvents";
 
-export { Dashboard, AboutUs, Programme };
+export { Dashboard, AboutUs, Programme, NewsAndEvents };
