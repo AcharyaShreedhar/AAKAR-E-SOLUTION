e15f627f8515be3aa47e3c55220a199c3c6f2ef3
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-28 22:26:36 +0545

    [SOC-13]creating gallery component

---
+++ sochcollege/src/Components/Gallery/index.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Gallery extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
+
+  render() {
+    return <div>Gallery</div>;
+  }
+}
+export default Gallery;
