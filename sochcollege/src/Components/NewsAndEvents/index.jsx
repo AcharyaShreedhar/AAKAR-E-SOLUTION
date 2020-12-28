d1a3d64f00b0235d4520107e50b911a5dc2ab700
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-28 22:17:57 +0545

    [SOC-12]Mapping of guest lectures

--- sochcollege/src/Components/NewsAndEvents/index.jsx
+++ sochcollege/src/Components/NewsAndEvents/index.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Image } from "react-bootstrap";
 
 class NewsAndEvents extends Component {
   constructor(props) {
@@ -9,7 +10,129 @@ class NewsAndEvents extends Component {
   }
 
   render() {
-    return <div>News And Events</div>;
+    return (
+      <div>
+        <div className="d-flex card">
+          <div className="d-flex p-3 bg-secondary">
+            <strong>News And Events</strong>
+          </div>
+          <p className="text-left color-light p-2">Guest Lectures</p>
+        </div>
+
+        <div className="d-flex flex-column">
+          <div className="d-flex card flex-row p-5 align-items-center">
+            <Image
+              className="w-25 p-1 rounded"
+              style={{ width: "16rem", height: "16rem" }}
+              src={"/images/gyanwali-1.jpg"}
+              alt=""
+            />
+            <Image
+              className="w-25 p-1 rounded"
+              style={{ width: "16rem", height: "16rem" }}
+              src={"/images/gyanwali-2.jpg"}
+              alt=""
+            />
+            <div className="w-50 d-flex flex-column text-left px-5">
+              <h3>Milan Gyawali</h3>
+              <p className="font-italic">
+                A Professional Trainer who is Pursuing his PHD in University of
+                Leicester UK has Conducted a Workshop for Faculties of Soch
+                College of IT Under the topic Learning and Teaching:
+                Transforming Tacit Knowledge.
+              </p>
+            </div>
+          </div>
+          <div className="d-flex card flex-row p-5 align-items-center">
+            <div className="w-50 d-flex flex-column text-left px-5">
+              <h3>Vabuk Pahari</h3>
+              <p className="font-italic">
+                Recipient of Davis Scholarship and currently studying Computer
+                Science at Wesleyan University, Connecticut, USA shares his
+                experience about studying in America, the application process
+                and finding the right college in the US for our students at
+                Soch.
+              </p>
+            </div>
+            <Image
+              className="w-25 p-1 rounded"
+              src={"/images/vabuk-1.jpg"}
+              alt=""
+            />
+            <Image
+              className="w-25 p-1 rounded"
+              src={"/images/vabuk-2.jpg"}
+              alt=""
+            />
+          </div>
+          <div className="d-flex card flex-row p-5 align-items-center">
+            <Image
+              className="w-25 p-1 rounded"
+              style={{ width: "16rem", height: "16rem" }}
+              src={"/images/lect1.jpg"}
+              alt=""
+            />
+            <Image
+              className="w-25 p-1 rounded"
+              style={{ width: "16rem", height: "16rem" }}
+              src={"/images/lect2.jpg"}
+              alt=""
+            />
+            <div className="w-50 d-flex flex-column text-left px-5">
+              <h3>Prof. Dr Albert Aukes</h3>
+              <p className="font-italic">
+                Former chief innovation officer of Deutsche Telekom AG and
+                Chairman of the supervisory board of the German Research Center
+                for Artificial Intelligence (GMBH) has an interaction with
+                students about the recent trends in technology.
+              </p>
+            </div>
+          </div>
+          <div className="d-flex card flex-row p-5 align-items-center">
+            <div className="w-50 d-flex flex-column text-left px-5">
+              <h3>Tristan Michael</h3>
+              <p className="font-italic">
+                A programmer from Disney USA shows students how to develop video
+                games applications.
+              </p>
+            </div>
+            <Image
+              className="w-25 p-1 rounded"
+              src={"/images/lect3.jpg"}
+              alt=""
+            />
+            <Image
+              className="w-25 p-1 rounded"
+              src={"/images/lect4.jpg"}
+              alt=""
+            />
+          </div>
+          <div className="d-flex card flex-row p-5 align-items-center">
+            <Image
+              className="w-25 p-1 rounded"
+              style={{ width: "16rem", height: "16rem" }}
+              src={"/images/lect5.jpg"}
+              alt=""
+            />
+            <Image
+              className="w-25 p-1 rounded"
+              style={{ width: "16rem", height: "16rem" }}
+              src={"/images/lect6.jpg"}
+              alt=""
+            />
+            <div className="w-50 d-flex flex-column text-left px-5">
+              <h3>Vaskar Pahari</h3>
+              <p className="font-italic">
+                A graduate student from Yale university and currently working as
+                Financial analyst at Brace Bridge Capital, Boston USA, has an
+                interaction with students about the significance of Algorithm in
+                Financial sector.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 export default NewsAndEvents;
