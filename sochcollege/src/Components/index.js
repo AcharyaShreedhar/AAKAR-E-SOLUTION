9ba1c3641d9fbaa72cbb78bc65179628b3b43569
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-29 08:57:54 +0545

    [SOC-13]fixing conflicts

--- sochcollege/src/Components/index.js
+++ sochcollege/src/Components/index.js
@@ -2,5 +2,6 @@ import Dashboard from "./Dashboard";
 import AboutUs from "./AboutUs";
 import Programme from "./Programmes";
 import NewsAndEvents from "./NewsAndEvents";
+import Gallery from "./Gallery";
 
-export { Dashboard, AboutUs, Programme, NewsAndEvents };
+export { Dashboard, AboutUs, Programme, NewsAndEvents, Gallery };
