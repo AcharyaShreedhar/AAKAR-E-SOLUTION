938c248816d5ced28ea8ea1b1d58f275504c3cde
Shreedhar Acharya <spacharya2537@gmail.com>
2020-12-30 14:17:33 +0545

    [SOC-15]integrating reactstrap and emailjs for feedback form

--- sochcollege/src/Components/ContactUs/index.jsx
+++ sochcollege/src/Components/ContactUs/index.jsx
@@ -1,13 +1,69 @@
 import React, { Component } from "react";
 import { Image } from "react-bootstrap";
+import * as emailjs from "emailjs-com";
+import {
+  Button,
+  FormFeedback,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+} from "reactstrap";
 
 class ContactUs extends Component {
   constructor(props) {
     super(props);
     this.state = {
       value: null,
+      name: "",
+      phoneNumber: "",
+      email: "",
+      subject: "",
+      message: "",
     };
   }
+  handleSubmit(e) {
+    e.preventDefault();
+    const { name, phoneNumber, email, subject, message } = this.state;
+    let templateParams = {
+      from_name: name,
+      from_phoneNumber: phoneNumber,
+      from_email: email,
+      to_name: "spacharya2537@gmail.com",
+      subject: subject,
+      message: message,
+    };
+
+    emailjs
+      .send(
+        "service_a701tti",
+        "template_kiafs64",
+        templateParams,
+        "user_71grJDF4CU0kQQdZsnnxM"
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+        },
+        (err) => {
+          console.log("FAILED...", err);
+        }
+      );
+
+    this.resetForm();
+  }
+  resetForm() {
+    this.setState({
+      name: "",
+      phoneNumber: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+  }
+  handleChange = (param, e) => {
+    this.setState({ [param]: e.target.value });
+  };
 
   render() {
     return (
@@ -18,12 +74,74 @@ class ContactUs extends Component {
           </div>
         </div>
         <div className="d-flex card">
-        <div className="d-flex p-2">
+          <div className="d-flex p-2">
             <strong>Location and Direction of Soch College</strong>
           </div>
           <Image className="rounded" src={"/images/sochmap.png"} alt="" />
         </div>
-        
+        <div>
+          <h1 className="p-heading1">Feedback</h1>
+          <Form onSubmit={this.handleSubmit.bind(this)}>
+            <FormGroup controlId="formBasicName">
+              <Label className="text-muted">Name</Label>
+              <Input
+                type="text"
+                name="name"
+                value={this.state.name}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, "name")}
+                placeholder="Name"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicName">
+              <Label className="text-muted">Phone Number</Label>
+              <Input
+                type="text"
+                name="phoneNumber"
+                value={this.state.phoneNumber}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, "phoneNumber")}
+                placeholder="Phone Number"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicEmail">
+              <Label className="text-muted">Email address</Label>
+              <Input
+                type="email"
+                name="email"
+                value={this.state.email}
+                className="text-primary"
+                onChange={this.handleChange.bind(this, "email")}
+                placeholder="Enter email"
+              />
+            </FormGroup>
+
+            <FormGroup controlId="formBasicSubject">
+              <Label className="text-muted">Subject</Label>
+              <Input
+                type="text"
+                name="subject"
+                className="text-primary"
+                value={this.state.subject}
+                onChange={this.handleChange.bind(this, "subject")}
+                placeholder="Subject"
+              />
+            </FormGroup>
+            <FormGroup controlId="formBasicMessage">
+              <Label className="text-muted">Feedback Message</Label>
+              <Input
+                type="textarea"
+                name="message"
+                className="text-primary"
+                value={this.state.message}
+                onChange={this.handleChange.bind(this, "message")}
+              />
+            </FormGroup>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </div>
       </div>
     );
   }
