95a30b1bf63d3e5dfb9d85fc94b42799b378917f
Shreedhar Acharya <spacharya2537@gmail.com>
2021-01-05 11:56:36 +0545

    [SOC-18]adding animation on scroll to different sections

--- sochcollege/src/Components/Gallery/index.jsx
+++ sochcollege/src/Components/Gallery/index.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Row, Col, Image, Container } from "react-bootstrap";
+import ScrollAnimation from "react-animate-on-scroll";
 
 class Gallery extends Component {
   constructor(props) {
@@ -21,124 +22,218 @@ class Gallery extends Component {
           <Container>
             <Row>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/sochcollege.JPG"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  animateOnce={true}
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/sochcollege.JPG"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
+              </Col>
+
+              <Col xs={6} md={4} className="py-2">
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/sports1.png"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
+              </Col>
+
+              <Col xs={6} md={4} className="py-2">
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  animateOnce
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/computerlab1.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/sports1.png"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/computerlab2.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/computerlab1.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/computerlab3.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/computerlab2.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/computerlab4.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/computerlab3.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/digitallab1.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/computerlab4.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/library1.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/digitallab1.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/library2.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/library1.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/library3.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/library2.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/library4.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/library3.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/canteen1.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/library4.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/canteen3.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/canteen1.jpg"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/canteen4.jpg"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
               <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/canteen3.jpg"}
-                  alt=""
-                  fluid
-                />
-              </Col>
-              <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/canteen4.jpg"}
-                  alt=""
-                  fluid
-                />
-              </Col>
-              <Col xs={6} md={4} className="py-2">
-                <Image
-                  className="rounded"
-                  src={"/images/canteen5.png"}
-                  alt=""
-                  fluid
-                />
+                <ScrollAnimation
+                  animateIn="zoomIn"
+                  duration={1}
+                  initiallyVisible={true}
+                >
+                  <Image
+                    className="rounded"
+                    src={"/images/canteen5.png"}
+                    alt=""
+                    fluid
+                  />
+                </ScrollAnimation>
               </Col>
             </Row>
           </Container>
